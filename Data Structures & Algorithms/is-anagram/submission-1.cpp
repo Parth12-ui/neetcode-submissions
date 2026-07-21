9b0f41a807773f78b555f19829d7a89a44b55718
Parth Nitesh Chaudhari <parthniteshchaudhari@gmail.com>
2026-07-21 06:41:54 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // if size is different, one of the string contain an additional character.
+        if (s.size() != t.size()) return 0;
+
+        //hashmap to track elements in each string in O[1] time and space complexity.
+        unordered_map<char, int> hash1;
+        unordered_map<char, int> hash2;
+
+        //track count of all the characters in s
+        for(char h : s){
+            if (hash1.count(h) == 0){
+                hash1[h] = 1;
+            }
+            else hash1[h]++;
+        }
+
+        // repeat for t
+        for(char h : t){
+            if (hash2.count(h) == 0){
+                hash2[h] = 1;
+            }
+            else hash2[h]++;
+        }
+
+        // check if all elements in hashmap have same count
+        for(auto& pair : hash1){
+            if (hash1[pair.first] != hash2[pair.first]) return 0;
+        }
+
+        return 1;
+    }
+};
