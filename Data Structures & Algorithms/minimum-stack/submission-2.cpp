23d80f1df0b9f68b893a84f9757cf55b0d107851
Parth Nitesh Chaudhari <parthniteshchaudhari@gmail.com>
2026-07-28 16:29:34 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+
+private:
+    stack<pair<int, int>> s;
+    int min;
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        if (s.empty()) min = val;
+
+        if (val < min) min = val;
+        s.emplace(val, min);
+    }
+    
+    void pop() {
+        s.pop();
+        if (!s.empty())min = s.top().second;
+    }
+    
+    int top() {
+        return s.top().first;
+    }
+    
+    int getMin() {
+        return s.top().second;
+    }
+};
